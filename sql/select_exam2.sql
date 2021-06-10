95cd56b5b8df99dafc05ca6c7da8f225238c0b16
kimeunbibi <84178283+kimeunbibi@users.noreply.github.com>
2021-06-10 15:21:53 +0900

    commit

---
+++ sql/select_exam2.sql
@@ -0,0 +1,41 @@
+--1 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
+--(1) 도서번호가1인도서의이름
+select bookname
+from Book
+where bookid=1
+;
+
+--(2) 가격이20,000원이상인도서의이름
+select bookname
+from Book
+where price>=20000
+;
+
+--2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
+--(3) 모든고객의이름, 주소
+select name, address
+from Customer
+;
+
+--(4) 2014년7월4일~7월7일사이에주문받은도서의주문번호
+select orderid
+from Orders
+where orderdate between '2014-07-04' and '2014-07-07'
+;
+
+--(5) 2014년7월4일~7월7일사이에주문받은도서를제외한도서의주문번호
+select orderid
+from Orders
+where orderdate not between '2014-07-04' and '2014-07-07'
+;
+--(6) 성이‘김’씨인고객의이름과주소
+select name, address
+from Customer
+where name like '김%'
+;
+
+--(7) 성이‘김’씨이고이름이‘아’로끝나는고객의이름과주소
+select name, address
+from Customer
+where name like '김%아'
+;
